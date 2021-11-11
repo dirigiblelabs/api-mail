7342f87b51d745fd513bd267ad77805026a50ad7
Ivan Volkov <61655573+iwolkov@users.noreply.github.com>
2021-11-11 17:33:33 +0200

    Update client.js
    replace var with let

--- mail/client.js
+++ mail/client.js
@@ -8,7 +8,7 @@
  * Contributors:
  *   SAP - initial API and implementation
  */
-var mail = require("mail/v4/client");
+let mail = require("mail/v4/client");
 for(var propertyName in mail) {
 	exports[propertyName] = mail[propertyName];
 }
